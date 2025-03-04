333010e71f22c8e69ec8ddcd4ce867bec2c2ccfc
shyamansi <66917662+shyamansi@users.noreply.github.com>
2025-03-04 12:06:48 +0530

    eleventh testcase - fail testcase

--- app/services/number_calculator.rb
+++ app/services/number_calculator.rb
@@ -1,6 +1,11 @@
 class NumberCalculator
 	def add(num)
 	return  0  if num.empty?
-	num.gsub("\n" ,",").split(',').map(&:to_i).sum
-	end
+	return numbers.to_i if numbers.match(/^\d+$/)
+
+    if numbers.start_with?("//")
+    delimiter, numbers = numbers[2..].split("\n", 2)
+    numbers = numbers.gsub(delimiter, ",")
+    end
+end
 end 
--- spec/services/number_calculator_spec.rb
+++ spec/services/number_calculator_spec.rb
@@ -21,6 +21,10 @@ require 'rails_helper'
 		it 'returns the sum of multiple numbers(having newline character in between) ' do
 		expect(NumberCalculator.new.add("1\n2,3")).to eq(6)
 		end
+
+		it 'returns the sum of multiple numbers(having custom delimeters) ' do
+		expect(NumberCalculator.new.add("//;\n1;2")).to eq(3)
+		end
      
 	end
 end
