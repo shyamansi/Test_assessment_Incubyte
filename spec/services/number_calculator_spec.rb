dba095cacda996fef10dd6d5a30748d7888b391a
shyamansi <66917662+shyamansi@users.noreply.github.com>
2025-03-04 11:26:14 +0530

    ninth testcase - fail testcase

--- spec/services/number_calculator_spec.rb
+++ spec/services/number_calculator_spec.rb
@@ -2,20 +2,25 @@ require 'rails_helper'
 
   RSpec.describe NumberCalculator, type: :service do
     describe '#add' do
-      it 'returns 0 for an empty string' do
-        expect(NumberCalculator.new.add("")).to eq(0)
-      end
+		it 'returns 0 for an empty string' do
+		expect(NumberCalculator.new.add("")).to eq(0)
+		end
 
-      it 'returns 1 for an 1 number' do
-        expect(NumberCalculator.new.add("1")).to eq(1)
-      end
+		it 'returns 1 for an 1 number' do
+		expect(NumberCalculator.new.add("1")).to eq(1)
+		end
 
-      it 'returns the sum of two numbers(comma separated)' do
-        expect(NumberCalculator.new.add("1,2")).to eq(3)
-      end
-      it 'returns the sum of multiple numbers' do
-        expect(NumberCalculator.new.add("1,2,3")).to eq(6)
-      end
+		it 'returns the sum of two numbers(comma separated)' do
+		expect(NumberCalculator.new.add("1,2")).to eq(3)
+		end
+
+		it 'returns the sum of multiple numbers (comma separated)' do
+		expect(NumberCalculator.new.add("1,2,3")).to eq(6)
+		end
+
+		it 'returns the sum of multiple numbers(having newline character in between) ' do
+		expect(NumberCalculator.new.add("1\n2,3")).to eq(6)
+		end
      
 	end
 end
